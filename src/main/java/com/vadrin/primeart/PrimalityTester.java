6e626bcdb75bb7a3d9f66e2b8bf9f80ff372ab3d
gv-prashanth <gvprashanth.android@gmail.com>
2018-04-09 23:33:34 +0530

    improved code

--- src/main/java/com/vadrin/primeart/PrimalityTester.java
+++ src/main/java/com/vadrin/primeart/PrimalityTester.java
@@ -1,60 +1,65 @@
 package com.vadrin.primeart;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PrimalityTester {
 
-	private static final Logger logger = LoggerFactory.getLogger(PrimeArtApplicationRunner.class);
-	
-	public void test(String input) {
-		//System.setProperty("webdriver.chrome.driver","D:\\Projects\\stsworkspace\\prime-art\\drivers\\chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-		System.setProperty("phantomjs.binary.path", "D:\\Projects\\stsworkspace\\prime-art\\drivers\\phantomjs.exe");
-		WebDriver driver = new PhantomJSDriver();
-		driver.get("https://www.alpertron.com.ar/ECM.HTM");
-		driver.findElement(By.id("value")).sendKeys(input);
-		driver.findElement(By.id("factor")).click();
-		for(int i=0;i<10;i++) {
-			try {
-				WebElement status = (new WebDriverWait(driver, 1))
-						.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"result\"]/ul/li")));
-				System.out.println(status.getText());
-				logger.info("PRIME: "+input);
-				break;
-			} catch (TimeoutException ex) {
-				try {
-					WebElement blue = (new WebDriverWait(driver, 1))
-							.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"result\"]/p[1]")));
-					System.out.println();
-					logger.info("COMPOSITE: "+input);
-					break;
-				} catch (TimeoutException e) {
+	@Autowired
+	private WebDriver driver;
 
-				}
-			}
-			if(i==29) {
-				System.out.println(input + " cant be determined");
-				logger.info("UNDETERMINED: "+input);
-			}else {
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+	private static final String PRIME = "PRIME";
+	private static final String COMPOSITE = "COMPOSITE";
+	private static final String UNDETERMINED = "UNDETERMINED";
+
+	public String test(String input) {
+
+		try {
+			WebElement stop = driver.findElement(By.xpath("//*[@id=\"stop\"]"));
+			stop.click();
+		} catch (ElementNotVisibleException ex) {
+
 		}
-		driver.close();
+
+		WebElement inputBox = (new WebDriverWait(driver, 10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"value\"]")));
+		inputBox.clear();
+		((JavascriptExecutor) driver).executeScript("arguments[0].value = arguments[1];", inputBox, input);
+		WebElement factor = (new WebDriverWait(driver, 10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"factor\"]")));
+		factor.click();
+
+		try {
+			WebElement compositeResult = (new WebDriverWait(driver, 10)).until(
+					ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"result\"]/p[@class=\"blue\"]")));
+			if (compositeResult.getText().toLowerCase().contains("digits) =")) {
+				return COMPOSITE;
+			}
+		} catch (TimeoutException ex) {
+
+		}
+
+		try {
+			WebElement primeResult = (new WebDriverWait(driver, 20))
+					.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"result\"]/ul/li")));
+			if (primeResult.getText().toLowerCase().contains("prime")) {
+				return PRIME;
+			} else {
+				return COMPOSITE;
+			}
+		} catch (TimeoutException ex) {
+			return UNDETERMINED;
+		}
+
 	}
+
 }
--- src/main/java/com/vadrin/primeart/PrimeArtApplication.java
+++ src/main/java/com/vadrin/primeart/PrimeArtApplication.java
@@ -1,12 +1,26 @@
 package com.vadrin.primeart;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeArtApplication {
 
+	@Value("${prime-art.drivers.chrome}")
+	private String chromeDriverLocation;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeArtApplication.class, args);
 	}
+
+	@Bean
+	public WebDriver getChromeDriver() {
+		System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+		return new ChromeDriver();
+	}
+
 }
